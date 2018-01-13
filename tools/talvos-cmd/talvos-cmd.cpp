8b1fbe1165aeaf39650f4fb075d37653cb7f703e
James Price <j.price@bristol.ac.uk>
2018-01-13 16:28:59 +0000

    Add LICENSE file and copyright headers

--- tools/talvos-cmd/talvos-cmd.cpp
+++ tools/talvos-cmd/talvos-cmd.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2018 the Talvos developers. All rights reserved.
+//
+// This file is distributed under a three-clause BSD license. For full license
+// terms please see the LICENSE file distributed with this source code.
+
 int main(int argc, char *argv[])
 {
   return 0;
