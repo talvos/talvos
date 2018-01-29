c6ab77673d354b53d53485efa1efb247e6e573c9
James Price <j.price@bristol.ac.uk>
2018-01-29 10:08:21 +0000

    Add Type::getElementCount() method
    Returns 1 for scalars.

--- include/talvos/Type.h
+++ include/talvos/Type.h
@@ -20,6 +20,7 @@ typedef std::vector<std::pair<const Type *, uint32_t>> StructElementTypeList;
 class Type
 {
 public:
+  uint32_t getElementCount() const { return ElementCount; };
   size_t getElementOffset(uint32_t Index) const;
   const Type *getElementType(uint32_t Index = 0) const;
   size_t getSize() const;
@@ -43,7 +44,11 @@ public:
   static Type *getVoid();
 
 private:
-  Type(uint32_t Id) { this->Id = Id; };
+  Type(uint32_t Id)
+  {
+    this->Id = Id;
+    ElementCount = 1;
+  };
 
   enum
   {
