1f53d11127bb19ce5c2354dd555100b1f7f63ccb
James Price <j.price@bristol.ac.uk>
2018-02-07 18:34:18 +0000

    Disable copy constructors for Function

--- include/talvos/Function.h
+++ include/talvos/Function.h
@@ -22,6 +22,10 @@ public:
   /// Create a new function with an ID and a type.
   Function(uint32_t Id, const Type *FunctionType);
 
+  // Do not allow Functions to be copied.
+  Function(const Function &) = delete;
+  Function &operator=(const Function &) = delete;
+
   /// Add a block to this function.
   void addBlock(std::unique_ptr<Block> B);
 
