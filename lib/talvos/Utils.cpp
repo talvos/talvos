72c55e1a84961682f28a7b11857932e3636c4396
James Price <j.price@bristol.ac.uk>
2018-02-27 12:28:20 +0000

    Add Utils.cpp file with checkEnv() function

---
+++ lib/talvos/Utils.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2018 the Talvos developers. All rights reserved.
+//
+// This file is distributed under a three-clause BSD license. For full license
+// terms please see the LICENSE file distributed with this source code.
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+namespace talvos
+{
+
+bool checkEnv(const char *Name, bool Default)
+{
+  const char *Value = getenv(Name);
+  if (!Value)
+    return Default;
+
+  if (!strcmp(Value, "0"))
+    return false;
+  else if (!strcmp(Value, "1"))
+    return true;
+
+  std::cerr << std::endl
+            << "ERROR: Invalid value for " << Name << " environment variable"
+            << std::endl;
+  abort();
+}
+
+} // namespace talvos
---
+++ lib/talvos/Utils.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 the Talvos developers. All rights reserved.
+//
+// This file is distributed under a three-clause BSD license. For full license
+// terms please see the LICENSE file distributed with this source code.
+
+#ifndef TALVOS_UTILS_H
+#define TALVOS_UTILS_H
+
+namespace talvos
+{
+
+/// Returns true if the environment variable \p Name is set to 1, false for 0,
+/// or \p Default if it is not set.
+bool checkEnv(const char *Name, bool Default);
+
+} // namespace talvos
+
+#endif
