7c7dd7368f3a633ea322ff17916804a823e57676
James Price <j.price@bristol.ac.uk>
2018-05-16 23:08:50 +0100

    Add RenderPassInstance class
    This class handles attachment initialization (e.g. clear values), and
    tracks the current subpass.

--- include/talvos/RenderPass.h
+++ include/talvos/RenderPass.h
@@ -70,6 +70,9 @@ public:
   /// Returns the attachment at index \p Index.
   const VkAttachmentDescription &getAttachment(uint32_t Index) const;
 
+  /// Returns the number of attachments in this render pass.
+  uint32_t getNumAttachments() const { return (uint32_t)Attachments.size(); }
+
   /// Returns the subpass at index \p Index.
   const Subpass &getSubpass(uint32_t Index) const;
 
@@ -79,6 +82,56 @@ private:
 
   /// The subpasses contained in this render pass.
   const std::vector<Subpass> Subpasses;
+};
+
+/// This class represents an instance of a render pass being used for drawing.
+class RenderPassInstance
+{
+public:
+  /// Create a render pass instance from a render pass and a framebuffer.
+  RenderPassInstance(const RenderPass &RP, const Framebuffer &FB,
+                     const std::vector<VkClearValue> &ClearValues)
+      : RP(RP), FB(FB), ClearValues(ClearValues), Rendering(false){};
+
+  /// Initialize the render pass state in preparation for draw commands.
+  void begin();
+
+  /// Finalize the render pass state after completing all draw commands.
+  void end();
+
+  /// Returns the framebuffer associated with this render pass instance.
+  const Framebuffer &getFramebuffer() const { return FB; }
+
+  /// Returns the index of the current subpass.
+  uint32_t getSubpassIndex() const { return SubpassIndex; }
+
+  /// Transition to the next subpass.
+  void nextSubpass();
+
+private:
+  /// The render pass that this object instantiates.
+  const RenderPass &RP;
+
+  /// The framebuffer associated with this render pass instance.
+  const Framebuffer &FB;
+
+  /// The clear values used for this render pass instance.
+  std::vector<VkClearValue> ClearValues;
+
+  /// Flag used to indicate whether the instance is currently rendering.
+  bool Rendering;
+
+  /// The index of the current subpass.
+  uint32_t SubpassIndex;
+
+  /// Flags denoting whether each attachment has been initialized yet.
+  std::vector<bool> AttachmentsInitialized;
+
+  /// Helper used to update render pass state when a new subpass is started.
+  void beginSubpass();
+
+  /// Helper used to update render pass state when a subpass completes.
+  void endSubpass();
 };
 
 }; // namespace talvos
--- lib/talvos/RenderPass.cpp
+++ lib/talvos/RenderPass.cpp
@@ -7,7 +7,10 @@
 /// This file defines the RenderPass class and related data structures.
 
 #include <cassert>
+#include <cmath>
 
+#include "talvos/Device.h"
+#include "talvos/Memory.h"
 #include "talvos/RenderPass.h"
 
 namespace talvos
@@ -23,6 +26,90 @@ const Subpass &RenderPass::getSubpass(uint32_t Index) const
 {
   assert(Index < Subpasses.size());
   return Subpasses[Index];
+}
+
+void RenderPassInstance::begin()
+{
+  // TODO: Mutex instead?
+  assert(!Rendering && "render pass overlapping not allowed");
+
+  Rendering = true;
+
+  AttachmentsInitialized.assign(RP.getNumAttachments(), false);
+
+  SubpassIndex = 0;
+  beginSubpass();
+}
+
+void RenderPassInstance::beginSubpass()
+{
+  // TODO: Handle stencil/preserve/resolve attachments too
+
+  const Subpass &Subpass = RP.getSubpass(SubpassIndex);
+
+  // Loop over color attachments in the subpass, clearing them as necessary.
+  for (uint32_t AttachIndex = 0; AttachIndex < Subpass.ColorAttachments.size();
+       AttachIndex++)
+  {
+    uint32_t AttachRef = Subpass.ColorAttachments[AttachIndex];
+
+    // Skip unused attachments and those that have been previously initialized.
+    if (AttachRef == VK_ATTACHMENT_UNUSED)
+      continue;
+    if (AttachmentsInitialized[AttachRef])
+      continue;
+
+    const VkAttachmentDescription &Attachment = RP.getAttachment(AttachRef);
+    AttachmentsInitialized[AttachRef] = true;
+
+    // Skip attachments if not clearing them.
+    if (Attachment.loadOp != VK_ATTACHMENT_LOAD_OP_CLEAR)
+      continue;
+
+    // TODO: Handle other image formats.
+    assert(Attachment.format == VK_FORMAT_R8G8B8A8_UNORM);
+
+    // Generate integer clear value.
+    assert(AttachRef < ClearValues.size());
+    const float *Clear = ClearValues[AttachRef].color.float32;
+    uint8_t Pixel[4] = {(uint8_t)std::round(Clear[0] * 255),
+                        (uint8_t)std::round(Clear[1] * 255),
+                        (uint8_t)std::round(Clear[2] * 255),
+                        (uint8_t)std::round(Clear[3] * 255)};
+
+    // Store clear value to each pixel in attachment.
+    for (uint32_t y = 0; y < FB.getHeight(); y++)
+    {
+      for (uint32_t x = 0; x < FB.getWidth(); x++)
+      {
+        uint64_t Address = FB.getAttachments()[AttachIndex];
+        Address += (x + (y * FB.getWidth())) * 4;
+        FB.getDevice().getGlobalMemory().store(Address, 4, Pixel);
+      }
+    }
+  }
+}
+
+void RenderPassInstance::end()
+{
+  assert(Rendering);
+
+  endSubpass();
+  Rendering = false;
+}
+
+void RenderPassInstance::endSubpass()
+{
+  // TODO: Perform multisample resolve operations if necessary
+}
+
+void RenderPassInstance::nextSubpass()
+{
+  assert(Rendering);
+
+  endSubpass();
+  ++SubpassIndex;
+  beginSubpass();
 }
 
 } // namespace talvos
