87ea350453d6ddcd66764f7e53e681aa00d0597b
James Price <j.price@bristol.ac.uk>
2018-06-24 13:56:48 +0100

    Implement vkGetImageMemoryRequirements2

--- lib/vulkan/resources.cpp
+++ lib/vulkan/resources.cpp
@@ -429,7 +429,46 @@ VKAPI_ATTR void VKAPI_CALL vkGetImageMemoryRequirements2(
     VkDevice device, const VkImageMemoryRequirementsInfo2 *pInfo,
     VkMemoryRequirements2 *pMemoryRequirements)
 {
-  TALVOS_ABORT_UNIMPLEMENTED;
+  vkGetImageMemoryRequirements(device, pInfo->image,
+                               &pMemoryRequirements->memoryRequirements);
+
+  {
+    // Walk through image requirement extensions.
+    const void *Ext = pInfo->pNext;
+    while (Ext)
+    {
+      switch (*(VkStructureType *)Ext)
+      {
+      default:
+        assert(false && "Unimplemented extension");
+      }
+
+      Ext = ((void **)Ext)[1];
+    }
+  }
+
+  {
+    // Walk through memory requirement extensions.
+    void *Ext = pMemoryRequirements->pNext;
+    while (Ext)
+    {
+      switch (*(VkStructureType *)Ext)
+      {
+      case VK_STRUCTURE_TYPE_MEMORY_DEDICATED_REQUIREMENTS:
+      {
+        VkMemoryDedicatedRequirements *Requirements =
+            (VkMemoryDedicatedRequirements *)Ext;
+        Requirements->prefersDedicatedAllocation = VK_FALSE;
+        Requirements->requiresDedicatedAllocation = VK_FALSE;
+        break;
+      }
+      default:
+        assert(false && "Unimplemented extension");
+      }
+
+      Ext = ((void **)Ext)[1];
+    }
+  }
 }
 
 VKAPI_ATTR void VKAPI_CALL vkGetImageMemoryRequirements2KHR(
